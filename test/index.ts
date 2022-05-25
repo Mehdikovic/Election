d2e002f06b00cb139105f776cccc0a9121566b70
MehdiZarei <alexmettkov@gmail.com>
2022-05-25 11:50:09 +0430

    initial commit

---
+++ test/index.ts
@@ -0,0 +1,131 @@
+import { expect } from "chai";
+import { Signer } from "ethers";
+import { ethers } from "hardhat";
+import { Election } from "../typechain";
+
+let ElectionFactory
+let election: Election
+
+let voter1: Signer
+let voter1Addr: string
+
+let voter2: Signer
+let voter2Addr: string
+
+let voter3: Signer
+let voter3Addr: string
+
+describe("Election", function () {
+  it("should deploy and add one candidate", async function () {
+    ElectionFactory = await ethers.getContractFactory("Election")
+    election = await ElectionFactory.deploy()
+    await election.deployed()
+
+    let tx = await election.registerCandidate("Name1", "Cult1", 20)
+    let rec = await tx.wait()
+    let id = rec.events?.[0].args?.id
+
+    expect(id).to.be.eq(1)
+
+    let candidate = await election.sortedCandidates(0)
+    expect(candidate.name).to.be.eq("Name1")
+    expect(candidate.cult).to.be.eq("Cult1")
+    expect(candidate.age).to.be.eq(20)
+    expect(candidate.votes).to.be.eq(0)
+
+    let accounts = await ethers.getSigners()
+    
+    voter1 = accounts[1]
+    voter1Addr = await voter1.getAddress()
+    
+    voter2 = accounts[2]
+    voter2Addr = await voter2.getAddress()
+    
+    voter3 = accounts[3]
+    voter3Addr = await voter3.getAddress()
+  });
+
+  it("should add two more candidates", async () => {
+    let tx = await election.registerCandidate("Name2", "Cult2", 25)
+    let rec = await tx.wait()
+    let id = rec.events?.[0].args?.id
+
+    expect(id).to.be.eq(2)
+    let candidate = await election.sortedCandidates(1)
+    expect(candidate.name).to.be.eq("Name2")
+    expect(candidate.cult).to.be.eq("Cult2")
+    expect(candidate.age).to.be.eq(25)
+    expect(candidate.votes).to.be.eq(0)
+
+    tx = await election.registerCandidate("Name3", "Cult3", 30)
+    rec = await tx.wait()
+    id = rec.events?.[0].args?.id
+
+    expect(id).to.be.eq(3)
+    candidate = await election.sortedCandidates(2)
+    expect(candidate.name).to.be.eq("Name3")
+    expect(candidate.cult).to.be.eq("Cult3")
+    expect(candidate.age).to.be.eq(30)
+    expect(candidate.votes).to.be.eq(0)
+  })
+
+  it("should fire an event for the first candidate", async () => {
+    let candidateId = 1
+
+    expect(await election.userCastedVote(voter1Addr, candidateId)).to.be.eq(false)
+
+    expect((await election.id2candidates(candidateId)).votes).to.be.eq(0)
+
+    await (await election.connect(voter1).castVote(candidateId)).wait()
+
+    expect((await election.getSortedCandidates())[0].votes).to.be.eq(1)
+    expect((await election.id2candidates(candidateId)).votes).to.be.eq(1)
+    expect(await election.userCastedVote(voter1Addr, candidateId)).to.be.eq(true)
+    expect((await election.getVotersOfCandidate(candidateId))[0]).to.be.eq(voter1Addr)
+    expect((await election.getCandidatesOfVoter(voter1Addr))[0]).to.be.eq(candidateId)
+  })
+
+  it("should fire an event for the first candidate", async () => {
+    let candidateId = 2
+
+    expect(await election.userCastedVote(voter1Addr, candidateId)).to.be.eq(false)
+    expect(await election.userCastedVote(voter2Addr, candidateId)).to.be.eq(false)
+
+    expect((await election.id2candidates(candidateId)).votes).to.be.eq(0)
+
+    await (await election.connect(voter1).castVote(candidateId)).wait()
+    await (await election.connect(voter2).castVote(candidateId)).wait()
+
+    expect((await election.getSortedCandidates())[0].votes).to.be.eq(2)
+    expect((await election.id2candidates(candidateId)).votes).to.be.eq(2)
+    expect(await election.userCastedVote(voter1Addr, candidateId)).to.be.eq(true)
+    expect(await election.userCastedVote(voter2Addr, candidateId)).to.be.eq(true)
+    expect((await election.getVotersOfCandidate(candidateId))[0]).to.be.eq(voter1Addr)
+    expect((await election.getVotersOfCandidate(candidateId))[1]).to.be.eq(voter2Addr)
+    expect((await election.getCandidatesOfVoter(voter1Addr))[1]).to.be.eq(candidateId)
+    expect((await election.getCandidatesOfVoter(voter2Addr))[0]).to.be.eq(candidateId)
+  })
+
+  it("should add candidate num 4", async () => {
+    let tx = await election.registerCandidate("Name4", "Cult4", 43)
+    let rec = await tx.wait()
+    let candidateId = rec.events?.[0].args?.id
+
+    expect(candidateId).to.be.eq(4)
+    let candidate = await election.sortedCandidates(3)
+    expect(candidate.name).to.be.eq("Name4")
+    expect(candidate.cult).to.be.eq("Cult4")
+    expect(candidate.age).to.be.eq(43)
+    expect(candidate.votes).to.be.eq(0)
+
+    rec = await (await election.connect(voter1).castVote(candidateId)).wait()
+    rec = await (await election.connect(voter2).castVote(candidateId)).wait()
+    rec = await (await election.connect(voter3).castVote(candidateId)).wait()
+    
+    expect((await election.id2candidates(candidateId)).votes).to.be.eq(3)
+    expect((await election.getSortedCandidates())[0].votes).to.be.eq(3)
+    expect((await election.getVotersOfCandidate(candidateId))[0]).to.be.eq(voter1Addr)
+    expect((await election.getVotersOfCandidate(candidateId))[1]).to.be.eq(voter2Addr)
+    expect((await election.getVotersOfCandidate(candidateId))[2]).to.be.eq(voter3Addr)
+  })
+});
